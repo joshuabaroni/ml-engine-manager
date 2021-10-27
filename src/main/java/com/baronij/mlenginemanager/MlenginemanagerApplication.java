725573f8bbe65b5b436086fe6db35633b188655b
joshuabaroni <joshua.baroni.16@cnu.edu>
2021-10-27 12:31:48 -0400

    first commit spring init

---
+++ src/main/java/com/baronij/mlenginemanager/MlenginemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.baronij.mlenginemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlenginemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlenginemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baronij/mlenginemanager/MlenginemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baronij.mlenginemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlenginemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
